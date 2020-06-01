e73de84cdf8a96466d944366d23f37e30e992248
AsciiBenefactor <asciibene@sdf.org>
2020-05-31 22:50:00 -0400

    update

--- lua/iotest.lua
+++ lua/iotest.lua
@@ -1,15 +1,29 @@
--- Basic file In/Out test
+-- Basic file In/Out test 
 -- asciibene 
-cls() 
-it = io.read("*a")
-fn = "/mnt/sdcard/test.txt" 
---open the file in fn and write "it"
+while isdone == false
+print("Please input desired text") 
+txt = io.read("*a") 
+tic()
+fn = "/mnt/sdcard/luatest" --filename 
+print("It will be written in ", fn) 
+--open the file in fn and write txt 
 fp = io.open(fn, "w+") 
-fp:write(it) 
-fp:close()
--- read the content to "tt"
+fp:write(txt) 
+fp:write("\n") 
+time = toc()
+fp:write("Elapsed time: ", time, "secs")
+fp:close() 
+-- read the content to "tt" 
 fp = io.open(fn, "r") 
 tt = fp:read("*all") 
 fp:close() 
--- tt to output
+-- tt to output 
 io.write(tt) 
+print ("Are you done ?")
+-- if done isdone becomes true
+di = io.read("*a")
+if di == y then isdone = true
+end
+
+
+
